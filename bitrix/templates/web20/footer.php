d098ad81d7fe13f70ca9bbf9190a90a6ef4575cc
HappinessCookie <faartur@gmail.com>
2015-02-16 20:47:36 +0500

    add footer and header file

---
+++ bitrix/templates/web20/footer.php
@@ -0,0 +1,9 @@
+<?
+if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+?> 
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="<?= SITE_TEMPLATE_PATH ?>/js/bootstrap.js"></script>
+</body>
+</html>

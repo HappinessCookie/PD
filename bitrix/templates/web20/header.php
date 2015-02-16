2fe29a7b37a63238070522f186cfc77c58fafaaa
HappinessCookie <faartur@gmail.com>
2015-02-16 21:49:26 +0500

    side-pad move

--- bitrix/templates/web20/header.php
+++ bitrix/templates/web20/header.php
@@ -22,10 +22,10 @@
 </head>
 <body>
 <div class="navbar navbar-inverse navbar-fixed-top">
-    <div class="container">
+    <div class="container side-pad">
         <div class="navbar-header">
-            <a href="#" class="navbar-brand side-pad">8-800-555-55-55</a>
-            <button type="button" class="navbar-toggle side-pad" data-toggle="collapse" data-target="#responsive-menu">
+            <a href="#" class="navbar-brand">8-800-555-55-55</a>
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#responsive-menu">
                 <span class="sr-only">Открыть навигацию</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
@@ -33,7 +33,7 @@
             </button>
         </div>
         <div class="collapse navbar-collapse" id="responsive-menu">
-            <ul class="nav navbar-nav navbar-right side-pad">
+            <ul class="nav navbar-nav navbar-right">
                 <li><a href="#">Главная</a></li>
                 <li><a href="#">О нас</a></li>
                 <li><a href="#">Готовые решения</a></li>

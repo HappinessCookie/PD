7438ad0f909af84d2912a4cc55073c913effce91
Денис <bikrevdeniis@mail.ru>
2015-02-16 19:01:42 +0300

    переименовал index некоторые изменения в шапке в подвале

--- bitrix/templates/web20/header.php
+++ bitrix/templates/web20/header.php
@@ -1,41 +1,42 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
+<!DOCTYPE html>
 <html>
 <head>
-<?$APPLICATION->ShowHead()?>
-<meta charset="utf-8">
+    <? $APPLICATION->ShowHead() ?>
+    <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>xD</title>
 
     <!-- Bootstrap -->
-	<link href="<?= SITE_TEMPLATE_PATH ?>/css/bootstrap.css" rel="stylesheet">
+    <link href="<?= SITE_TEMPLATE_PATH ?>/css/bootstrap.css" rel="stylesheet">
     <link href="<?= SITE_TEMPLATE_PATH ?>/css/font-awesome.css" rel="stylesheet">
     <link href="<?= SITE_TEMPLATE_PATH ?>/css/style.css" rel="stylesheet">
     <link href="<?= SITE_TEMPLATE_PATH ?>/css/responsive3d.css" rel="stylesheet">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 </head>
 
 <body>
 <div class="navbar navbar-inverse navbar-fixed-top container">
-       <div class="navbar-header">
-          <a href="#" class="navbar-brand">8-800-555-55-55</a>
-			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#responsive-menu">
-			  <span class="sr-only">Открыть навигацию</span>
-			  <span class="icon-bar"></span>
-			  <span class="icon-bar"></span>
-			  <span class="icon-bar"></span>            
-			</button>
-       </div>
-		<div class="collapse navbar-collapse" id="responsive-menu">
-		 <ul class="nav navbar-nav pull-right">
-				<li><a href="#">Главная</a></li>
-				<li><a href="#">О нас</a></li>
-				<li><a href="#">Готовое решение</a></li>  
-		 </ul>  
-		</div>
-	</div>
+    <div class="navbar-header">
+        <a href="#" class="navbar-brand">8-800-555-55-55</a>
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#responsive-menu">
+            <span class="sr-only">РћС‚РєСЂС‹С‚СЊ РЅР°РІРёРіР°С†РёСЋ</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+    </div>
+    <div class="collapse navbar-collapse" id="responsive-menu">
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="#">Р“Р»Р°РІРЅР°СЏ</a></li>
+            <li><a href="#">Рћ РЅР°СЃ</a></li>
+            <li><a href="#">Р“РѕС‚РѕРІС‹Рµ СЂРµС€РµРЅРёСЏ</a></li>
+        </ul>
+    </div>
+</div>

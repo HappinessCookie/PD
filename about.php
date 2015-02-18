9747f4bb4530cda4aa377c4e0e0b12d55e720ebd
HappinessCookie <faartur@gmail.com>
2015-02-18 23:59:28 +0500

    First plugin test

---
+++ about.php
@@ -0,0 +1,168 @@
+<div id="page" class="container">
+    <div class="header">
+        <img src="/bitrix/templates/web20/images/prizent.jpg" alt="" class="img-responsive">
+    </div>
+    <!--MIN-PORTFOLIO-->
+    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+        <div class="xoverlay x-top-red">
+            <img src="http://placehold.it/300" alt="" class="image-responsive">
+            <div class="xoverlay-box">
+                <div class="xoverlay-data">
+                    <h1 class="data-info">Text</h1>
+                    <p class="data-info">Text</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+        <div class="xoverlay x-top-blue">
+            <img src="http://placehold.it/300" alt="" class="image-responsive">
+            <div class="xoverlay-box">
+                <div class="xoverlay-data">
+                    <h1 class="data-info">Text</h1>
+                    <p class="data-info">Text</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+        <div class="xoverlay x-top-yellow">
+            <img src="http://placehold.it/300" alt="" class="image-responsive">
+            <div class="xoverlay-box">
+                <div class="xoverlay-data">
+                    <h1 class="data-info">Text</h1>
+                    <p class="data-info">Text</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+        <div class="xoverlay x-top-pink">
+            <img src="http://placehold.it/300" alt="" class="image-responsive">
+            <div class="xoverlay-box">
+                <div class="xoverlay-data">
+                    <h1 class="data-info">Text</h1>
+                    <p class="data-info">Text</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+        <div class="xoverlay x-top-darkyellow">
+            <img src="http://placehold.it/300" alt="" class="image-responsive">
+            <div class="xoverlay-box">
+                <div class="xoverlay-data">
+                    <h1 class="data-info">Text</h1>
+                    <p class="data-info">Text</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+        <div class="xoverlay x-top-lightpink">
+            <img src="http://placehold.it/300" alt="" class="image-responsive">
+            <div class="xoverlay-box">
+                <div class="xoverlay-data">
+                    <h1 class="data-info">Text</h1>
+                    <p class="data-info">Text</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+        <div class="xoverlay x-top-brown">
+            <img src="http://placehold.it/300" alt="" class="image-responsive">
+            <div class="xoverlay-box">
+                <div class="xoverlay-data">
+                    <h1 class="data-info">Text</h1>
+                    <p class="data-info">Text</p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+        <div class="xoverlay x-top-red">
+            <img src="http://placehold.it/300" alt="" class="image-responsive">
+            <div class="xoverlay-box">
+                <div class="xoverlay-data">
+                    <h1 class="data-info">Text</h1>
+                    <p class="data-info">Text</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+        <div class="xoverlay x-top-lightblue">
+            <img src="http://placehold.it/300" alt="" class="image-responsive">
+            <div class="xoverlay-box">
+                <div class="xoverlay-data">
+                    <h1 class="data-info">Text</h1>
+                    <p class="data-info">Text</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+        <div class="xoverlay x-top-yellow">
+            <img src="http://placehold.it/300" alt="" class="image-responsive">
+            <div class="xoverlay-box">
+                <div class="xoverlay-data">
+                    <h1 class="data-info">Text</h1>
+                    <p class="data-info">Text</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+        <div class="xoverlay x-top-pink">
+            <img src="http://placehold.it/300" alt="" class="image-responsive">
+            <div class="xoverlay-box">
+                <div class="xoverlay-data">
+                    <h1 class="data-info">Text</h1>
+                    <p class="data-info">Text</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+        <div class="xoverlay x-top-lightpink">
+            <img src="http://placehold.it/300" alt="" class="image-responsive">
+            <div class="xoverlay-box">
+                <div class="xoverlay-data">
+                    <h1 class="data-info">Text</h1>
+                    <p class="data-info">Text</p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--POL LITRA-->
+    <div class="section">
+        <img src="http://placehold.it/1200x300/d5d5d5/000" alt="" class="img-responsive">
+    </div>
+    <!--PARTNERS-->
+    <div class="section">
+        <img src="http://placehold.it/1200x200/454545/000" alt="" class="img-responsive">
+        <img src="http://placehold.it/1200x200/454545/000" alt="" class="img-responsive">
+        <img src="http://placehold.it/1200x200/454545/000" alt="" class="img-responsive">
+    </div>
+    <!--CONTACTS-->
+    <img src="http://placehold.it/1200x150/545454/000" alt="" class="img-responsive">
+    <div class="col-md-4 col-xs-4">
+        <p class="social"><a href=""><i class="fa fa-vk fa-5x"></i></a></p>
+    </div>
+    <div class="col-md-4 col-xs-4">
+        <p class="social"><a href=""><i class="fa fa-facebook fa-5x"></i></a></p>
+    </div>
+    <div class="col-md-4 col-xs-4">
+        <p class="social"><a href=""><i class="fa fa-twitter fa-5x"></i></a></p>
+    </div>
+    <!--MAP-->
+    <div class="section">
+        <div id="map"></div>
+        <script type="text/javascript"
+                charset="utf-8"
+                src="//api-maps.yandex.ru/services/constructor/1.0/js/?							sid=2tsE6AH7M4bd1oRgtS2xUrHRiuUn17OE&width=100%&height=400">
+        </script>
+    </div>
+</div>
--- bitrix/templates/web20/footer.php
+++ bitrix/templates/web20/footer.php
@@ -5,5 +5,7 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="<?= SITE_TEMPLATE_PATH ?>/js/bootstrap.js"></script>
+<script src="<?= SITE_TEMPLATE_PATH ?>/js/pagify.js"></script>
+<script src="<?= SITE_TEMPLATE_PATH ?>/js/function.js"></script>
 </body>
 </html>

f368ff2415cef1124a09f2e4127d79e9f7d4b556
Денис <bikrevdeniis@mail.ru>
2015-02-24 18:31:17 +0300

    change animation stickerchange margin - page-holder                         navbar - border: none                         change sticker color

--- about.php
+++ about.php
@@ -6,7 +6,23 @@
     <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
         <div class="sticker sticker-top">
             <img src="http://placehold.it/300" alt="" class="image-responsive">
-            <div class="sticker-box sticker-darkred">
+            <div class="sticker-box sticker-red">
+
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+        <div class="sticker sticker-top">
+            <img src="http://placehold.it/300" alt="" class="image-responsive">
+            <div class="sticker-box sticker-blue">
+
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+        <div class="sticker sticker-top">
+            <img src="http://placehold.it/300" alt="" class="image-responsive">
+            <div class="sticker-box sticker-darkyellow">
 
             </div>
         </div>
@@ -22,7 +38,7 @@
     <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
         <div class="sticker sticker-top">
             <img src="http://placehold.it/300" alt="" class="image-responsive">
-            <div class="sticker-box sticker-darkred">
+            <div class="sticker-box sticker-blue">
 
             </div>
         </div>
@@ -30,7 +46,7 @@
     <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
         <div class="sticker sticker-top">
             <img src="http://placehold.it/300" alt="" class="image-responsive">
-            <div class="sticker-box sticker-darkred">
+            <div class="sticker-box sticker-blue">
 
             </div>
         </div>
@@ -38,7 +54,7 @@
     <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
         <div class="sticker sticker-top">
             <img src="http://placehold.it/300" alt="" class="image-responsive">
-            <div class="sticker-box sticker-darkred">
+            <div class="sticker-box sticker-pink">
 
             </div>
         </div>
@@ -46,7 +62,7 @@
     <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
         <div class="sticker sticker-top">
             <img src="http://placehold.it/300" alt="" class="image-responsive">
-            <div class="sticker-box sticker-darkred">
+            <div class="sticker-box sticker-brown">
 
             </div>
         </div>
@@ -54,7 +70,7 @@
     <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
         <div class="sticker sticker-top">
             <img src="http://placehold.it/300" alt="" class="image-responsive">
-            <div class="sticker-box sticker-darkred">
+            <div class="sticker-box sticker-red">
 
             </div>
         </div>
@@ -62,7 +78,7 @@
     <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
         <div class="sticker sticker-top">
             <img src="http://placehold.it/300" alt="" class="image-responsive">
-            <div class="sticker-box sticker-darkred">
+            <div class="sticker-box sticker-lightblue">
 
             </div>
         </div>
@@ -70,7 +86,7 @@
     <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
         <div class="sticker sticker-top">
             <img src="http://placehold.it/300" alt="" class="image-responsive">
-            <div class="sticker-box sticker-darkred">
+            <div class="sticker-box ticker-brown">
 
             </div>
         </div>
@@ -78,36 +94,20 @@
     <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
         <div class="sticker sticker-top">
             <img src="http://placehold.it/300" alt="" class="image-responsive">
-            <div class="sticker-box sticker-darkred">
-
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
-        <div class="sticker sticker-top">
-            <img src="http://placehold.it/300" alt="" class="image-responsive">
-            <div class="sticker-box sticker-darkred">
-
-            </div>
-        </div>
-    </div>
-    <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
-        <div class="sticker sticker-top">
-            <img src="http://placehold.it/300" alt="" class="image-responsive">
-            <div class="sticker-box sticker-darkred">
+            <div class="sticker-box sticker-blue">
 
             </div>
         </div>
     </div>
     <!--POL LITRA-->
     <div class="section">
-        <img src="http://placehold.it/1200x300/d5d5d5/000" alt="" class="img-responsive">
+        <img src="http://placehold.it/1000x300/d5d5d5/000" alt="" class="img-responsive">
     </div>
     <!--PARTNERS-->
     <div class="section">
-        <img src="http://placehold.it/1200x200/454545/000" alt="" class="img-responsive">
-        <img src="http://placehold.it/1200x200/454545/000" alt="" class="img-responsive">
-        <img src="http://placehold.it/1200x200/454545/000" alt="" class="img-responsive">
+        <img src="http://placehold.it/1100x200/454545/000" alt="" class="img-responsive">
+        <img src="http://placehold.it/1100x200/454545/000" alt="" class="img-responsive">
+        <img src="http://placehold.it/1100x200/454545/000" alt="" class="img-responsive">
     </div>
     <!--CONTACTS-->
     <img src="http://placehold.it/1200x150/545454/000" alt="" class="img-responsive">
@@ -125,7 +125,7 @@
         <div id="map"></div>
         <script type="text/javascript"
                 charset="utf-8"
-                src="//api-maps.yandex.ru/services/constructor/1.0/js/?							sid=2tsE6AH7M4bd1oRgtS2xUrHRiuUn17OE&width=100%&height=400">
+                src="//api-maps.yandex.ru/services/constructor/1.0/js/?sid=2tsE6AH7M4bd1oRgtS2xUrHRiuUn17OE&width=100%&height=400">
         </script>
     </div>
 </div>

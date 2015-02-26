d59a1dd11bb1a5c26c76fafd306e5a5b30cef337
HappinessCookie <faartur@gmail.com>
2015-02-27 03:03:57 +0500

    Load 3 test page

--- index.php
+++ index.php
@@ -5,5 +5,338 @@ $APPLICATION->SetPageProperty("NOT_SHOW_NAV_CHAIN", "Y");
 $APPLICATION->SetTitle("Главная страница");
 ?>
     <!--GENERAL-IMAGE-->
-    <div id="page_holder"></div>
+    <div class="owl-carousel">
+        <div data-hash="main" class="item">
+            <div id="page" class="container">
+                <div class="header">
+                    <img src="http://placehold.it/1200x600" alt="" class="img-responsive">
+                </div>
+                <!--MIN-PORTFOLIO-->
+                <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+                    <div class="sticker sticker-top">
+                        <img src="/bitrix/templates/web20/images/section/1.jpg" alt="" class="image-responsive">
+                        <a href="#carousel" class="goto" data-slide="next">
+                            <div class="sticker-box sticker-color1"></div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+                    <div class="sticker sticker-top">
+                        <img src="/bitrix/templates/web20/images/section/2.jpg" alt="" class="image-responsive">
+                        <a href="#carousel" class="goto" data-slide="next">
+                            <div class="sticker-box sticker-color1"></div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+                    <div class="sticker sticker-top">
+                        <img src="/bitrix/templates/web20/images/section/3.jpg" alt="" class="image-responsive">
+                        <a href="#carousel" class="goto" data-slide="next">
+                            <div class="sticker-box sticker-color1"></div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+                    <div class="sticker sticker-top">
+                        <img src="/bitrix/templates/web20/images/section/4.jpg" alt="" class="image-responsive">
+                        <a href="#carousel" class="goto" data-slide="next">
+                            <div class="sticker-box sticker-color1"></div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+                    <div class="sticker sticker-top">
+                        <img src="/bitrix/templates/web20/images/section/5.jpg" alt="" class="image-responsive">
+                        <a href="#carousel" class="goto" data-slide="next">
+                            <div class="sticker-box sticker-color1"></div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+                    <div class="sticker sticker-top">
+                        <img src="/bitrix/templates/web20/images/section/6.jpg" alt="" class="image-responsive">
+                        <a href="#carousel" class="goto" data-slide="next">
+                            <div class="sticker-box sticker-color1"></div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+                    <div class="sticker sticker-top">
+                        <img src="/bitrix/templates/web20/images/section/7.jpg" alt="" class="image-responsive">
+                        <a href="#carousel" class="goto" data-slide="next">
+                            <div class="sticker-box sticker-color1"></div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+                    <div class="sticker sticker-top">
+                        <img src="/bitrix/templates/web20/images/section/8.jpg" alt="" class="image-responsive">
+                        <a href="#carousel" class="goto" data-slide="next">
+                            <div class="sticker-box sticker-color1"></div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+                    <div class="sticker sticker-top">
+                        <img src="/bitrix/templates/web20/images/section/9.jpg" alt="" class="image-responsive">
+                        <a href="#carousel" class="goto" data-slide="next">
+                            <div class="sticker-box sticker-color1"></div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+                    <div class="sticker sticker-top">
+                        <img src="/bitrix/templates/web20/images/section/10.jpg" alt="" class="image-responsive">
+                        <a href="#carousel" class="goto" data-slide="next">
+                            <div class="sticker-box sticker-color1"></div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+                    <div class="sticker sticker-top">
+                        <img src="/bitrix/templates/web20/images/section/11.jpg" alt="" class="image-responsive">
+                        <a href="#carousel" class="goto" data-slide="next">
+                            <div class="sticker-box sticker-color1"></div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+                    <div class="sticker sticker-top">
+                        <img src="/bitrix/templates/web20/images/section/12.jpg" alt="" class="image-responsive">
+                        <a href="#carousel" class="goto" data-slide="next">
+                            <div class="sticker-box sticker-color1"></div>
+                        </a>
+                    </div>
+                </div>
+                <!--POL LITRA-->
+                <div class="section"></div>
+                <!--PARTNERS-->
+                <div class="section">
+                    <img src="http://placehold.it/1200x200/454545/000" alt="" class="img-responsive">
+                    <img src="http://placehold.it/1200x200/454545/000" alt="" class="img-responsive">
+                    <img src="http://placehold.it/1200x200/454545/000" alt="" class="img-responsive">
+                </div>
+                <!--CONTACTS-->
+                <img src="http://placehold.it/1200x150/545454/000" alt="" class="img-responsive">
+                <div class="col-md-4 col-xs-4">
+                    <p class="social"><a href=""><i class="fa fa-vk fa-5x"></i></a></p>
+                </div>
+                <div class="col-md-4 col-xs-4">
+                    <p class="social"><a href=""><i class="fa fa-facebook fa-5x"></i></a></p>
+                </div>
+                <div class="col-md-4 col-xs-4">
+                    <p class="social"><a href=""><i class="fa fa-twitter fa-5x"></i></a></p>
+                </div>
+                <!--MAP-->
+                <div class="section">
+                    <div id="map"></div>
+                    <script type="text/javascript"
+                            charset="utf-8"
+                            src="//api-maps.yandex.ru/services/constructor/1.0/js/?sid=2tsE6AH7M4bd1oRgtS2xUrHRiuUn17OE&width=100%&height=400">
+                    </script>
+                </div>
+            </div>
+        </div>
+        <div data-hash="about" class="item">
+            <div id="page" class="container">
+                <div class="header">
+                    <img src="http://placehold.it/1200x600" alt="" class="img-responsive">
+                </div>
+                <!--MIN-PORTFOLIO-->
+                <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+                    <div class="sticker sticker-top">
+                        <img src="http://placehold.it/300/000" alt="" class="image-responsive">
+                        <div class="sticker-box sticker-red">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+                    <div class="sticker sticker-top">
+                        <img src="http://placehold.it/300/000" alt="" class="image-responsive">
+                        <div class="sticker-box sticker-blue">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+                    <div class="sticker sticker-top">
+                        <img src="http://placehold.it/300/000" alt="" class="image-responsive">
+                        <div class="sticker-box sticker-darkyellow">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+                    <div class="sticker sticker-top">
+                        <img src="http://placehold.it/300/000" alt="" class="image-responsive">
+                        <div class="sticker-box sticker-darkred">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+                    <div class="sticker sticker-top">
+                        <img src="http://placehold.it/300/000" alt="" class="image-responsive">
+                        <div class="sticker-box sticker-blue">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+                    <div class="sticker sticker-top">
+                        <img src="http://placehold.it/300/000" alt="" class="image-responsive">
+                        <div class="sticker-box sticker-blue">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+                    <div class="sticker sticker-top">
+                        <img src="http://placehold.it/300/000" alt="" class="image-responsive">
+                        <div class="sticker-box sticker-pink">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+                    <div class="sticker sticker-top">
+                        <img src="http://placehold.it/300/000" alt="" class="image-responsive">
+                        <div class="sticker-box sticker-brown">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+                    <div class="sticker sticker-top">
+                        <img src="http://placehold.it/300/000" alt="" class="image-responsive">
+                        <div class="sticker-box sticker-red">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+                    <div class="sticker sticker-top">
+                        <img src="http://placehold.it/300/000" alt="" class="image-responsive">
+                        <div class="sticker-box sticker-lightblue">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+                    <div class="sticker sticker-top">
+                        <img src="http://placehold.it/300/000" alt="" class="image-responsive">
+                        <div class="sticker-box sticker-brown">
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-4 col-xs-6">
+                    <div class="sticker sticker-top">
+                        <img src="http://placehold.it/300/000" alt="" class="image-responsive">
+                        <div class="sticker-box sticker-blue">
+
+                        </div>
+                    </div>
+                </div>
+                <!--POL LITRA-->
+                <div class="section">
+
+                </div>
+                <!--PARTNERS-->
+                <div class="section">
+                    <img src="http://placehold.it/1200x200/454545/000" alt="" class="img-responsive">
+                    <img src="http://placehold.it/1200x200/454545/000" alt="" class="img-responsive">
+                    <img src="http://placehold.it/1200x200/454545/000" alt="" class="img-responsive">
+                </div>
+                <!--CONTACTS-->
+                <img src="http://placehold.it/1200x150/545454/000" alt="" class="img-responsive">
+                <div class="col-md-4 col-xs-4">
+                    <p class="social"><a href=""><i class="fa fa-vk fa-5x"></i></a></p>
+                </div>
+                <div class="col-md-4 col-xs-4">
+                    <p class="social"><a href=""><i class="fa fa-facebook fa-5x"></i></a></p>
+                </div>
+                <div class="col-md-4 col-xs-4">
+                    <p class="social"><a href=""><i class="fa fa-twitter fa-5x"></i></a></p>
+                </div>
+                <!--MAP-->
+                <div class="section">
+                    <div id="map"></div>
+                    <script type="text/javascript"
+                            charset="utf-8"
+                            src="//api-maps.yandex.ru/services/constructor/1.0/js/?sid=2tsE6AH7M4bd1oRgtS2xUrHRiuUn17OE&width=100%&height=400">
+                    </script>
+                </div>
+            </div>
+        </div>
+        <div data-hash="finish" class="item">
+            <div id="page" class="container">
+                <?$APPLICATION->IncludeComponent(
+                    "bitrix:news.list",
+                    "jobs",
+                    Array(
+                        "IBLOCK_TYPE" => "-",
+                        "IBLOCK_ID" => "1",
+                        "NEWS_COUNT" => "20",
+                        "SORT_BY1" => "",
+                        "SORT_ORDER1" => "",
+                        "SORT_BY2" => "",
+                        "SORT_ORDER2" => "",
+                        "FILTER_NAME" => "",
+                        "FIELD_CODE" => array(
+                            "PROPERTY_p1",
+                            "PROPERTY_p2",
+                            "PROPERTY_p3",
+                            "PROPERTY_p4",
+                            "PROPERTY_p5",
+                            "PROPERTY_p6",
+                            "PROPERTY_p7",
+                            "PROPERTY_p8",
+                            "PROPERTY_p9",
+                            "PROPERTY_p10",
+                            "PROPERTY_p11",
+                            "PROPERTY_p12",
+                            "PROPERTY_p13",
+                            "PROPERTY_p14",
+                            "PROPERTY_p15"
+                        ),
+                        "PROPERTY_CODE" => array("", ""),
+                        "CHECK_DATES" => "Y",
+                        "DETAIL_URL" => "",
+                        "AJAX_MODE" => "N",
+                        "AJAX_OPTION_JUMP" => "N",
+                        "AJAX_OPTION_STYLE" => "Y",
+                        "AJAX_OPTION_HISTORY" => "N",
+                        "CACHE_TYPE" => "A",
+                        "CACHE_TIME" => "36000000",
+                        "CACHE_FILTER" => "N",
+                        "CACHE_GROUPS" => "Y",
+                        "PREVIEW_TRUNCATE_LEN" => "",
+                        "ACTIVE_DATE_FORMAT" => "",
+                        "SET_STATUS_404" => "N",
+                        "SET_TITLE" => "Y",
+                        "INCLUDE_IBLOCK_INTO_CHAIN" => "Y",
+                        "ADD_SECTIONS_CHAIN" => "Y",
+                        "HIDE_LINK_WHEN_NO_DETAIL" => "N",
+                        "PARENT_SECTION" => "",
+                        "PARENT_SECTION_CODE" => "",
+                        "INCLUDE_SUBSECTIONS" => "Y",
+                        "DISPLAY_DATE" => "Y",
+                        "DISPLAY_NAME" => "Y",
+                        "DISPLAY_PICTURE" => "Y",
+                        "DISPLAY_PREVIEW_TEXT" => "Y",
+                        "PAGER_TEMPLATE" => "",
+                        "DISPLAY_TOP_PAGER" => "N",
+                        "DISPLAY_BOTTOM_PAGER" => "Y",
+                        "PAGER_TITLE" => "Новости",
+                        "PAGER_SHOW_ALWAYS" => "Y",
+                        "PAGER_DESC_NUMBERING" => "N",
+                        "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
+                        "PAGER_SHOW_ALL" => "Y"
+                    )
+                );?>
+            </div>
+        </div>
+    </div>
 <? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>

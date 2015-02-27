77edee68e65d50a9f806373773bc765d35a11c7c
HappinessCookie <faartur@gmail.com>
2015-02-28 00:50:00 +0500

    Add palette. Page carousel changed

--- bitrix/templates/web20/js/function.js
+++ bitrix/templates/web20/js/function.js
@@ -1,20 +1,29 @@
 $(document).ready(function () {
-    var owl = $('.owl-carousel');
+    var owl = $('.owl-carousel')
     owl.owlCarousel({
         loop: false,
         items: 1,
         URLhashListener: true,
-        startPosition: 'URLHash'
+        startPosition: "#main"
     });
-    owl.on('dragged.owl.carousel', function(event) {
+    owl.on('changed.owl.carousel', function (event) {
         switch (event.relatedTarget._current) {
-            case 0: window.history.pushState({url:'/#main'}, "", "/#main");
+            case 0:
+                window.history.pushState("string", "", "/#main");
+                location.hash = "main";
                 break;
-            case 1: window.history.pushState({url:'/#about'}, "", "/#about");
+            case 1:
+                window.history.pushState("string", "", "/#about");
+                location.hash = "about";
                 break;
-            case 2: window.history.pushState({url:'/#finish'}, "", "/#finish");
+            case 2:
+                window.history.pushState("string", "", "/#finish");
+                location.hash = "finish";
                 break;
         }
-        console.log(event);
+        console.log(event.item.index);
+    });
+    $(".palette").click(function() {
+        $("#main-carousel").css("background-color", $(this).data("color"));
     });
 });

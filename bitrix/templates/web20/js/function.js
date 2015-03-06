d47e2d911ee6af774f642febbb439dcfcb2936ef
HappinessCookie <faartur@gmail.com>
2015-03-06 15:39:38 +0500

    Plugin start

--- bitrix/templates/web20/js/function.js
+++ bitrix/templates/web20/js/function.js
@@ -1,28 +1,12 @@
 $(document).ready(function () {
-    var owl = $('.owl-carousel');
-    owl.owlCarousel({
-        loop: false,
-        items: 1,
-        URLhashListener: true,
-        startPosition: "URLHash",
-        autoHeight: true
-    });
-    owl.on('changed.owl.carousel', function (event) {
-        switch (event.relatedTarget._current) {
-            case 0:
-                window.history.pushState("string", "", "/#main");
-                location.hash = "main";
-                break;
-            case 1:
-                window.history.pushState("string", "", "/#about");
-                location.hash = "about";
-                break;
-            case 2:
-                window.history.pushState("string", "", "/#finish");
-                location.hash = "finish";
-                break;
-        }
-        console.log(event.item.index);
+    $('.slider-pro').sliderPro({
+        width: "100%",
+        height: "100%",
+        autoHeight: true,
+        autoplay: false,
+        updateHash: true,
+        buttons: false,
+        loop: false
     });
     $(".palette").click(function () {
         $("#main-carousel").css("background-color", $(this).data("color"));

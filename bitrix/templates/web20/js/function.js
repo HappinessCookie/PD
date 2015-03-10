b1314cd00b50943254a9f975cf59c3215ac15534
HappinessCookie <faartur@gmail.com>
2015-03-10 13:16:41 +0500

    Link fix

--- bitrix/templates/web20/js/function.js
+++ bitrix/templates/web20/js/function.js
@@ -7,13 +7,35 @@ $(document).ready(function () {
         loop: false,
         forceSize: "fullWindow"
     });
-    $('#partners-slider').sliderPro();
-    $(".palette-link").click(function () {
-        $("body").css("background-color", $(this).data("color"));
-    });
-    $(".dial").knob({
-        format: function(v) {
-            return v + "%";
+    /*$('#partners-slider').sliderPro();*/
+});
+$(window).load(function () {
+    goto = function (to) {
+        $('html,body').animate({
+            scrollTop: $(to).offset().top
+        }, 500);
+    };
+    $(".anchor").click(function () {
+        if ($(this).data("page") !== "undefined") {
+            var page_index = 0;
+            switch ($(this).data("page")) {
+                case "main":
+                    page_index = 0;
+                    break;
+                case "about":
+                    page_index = 1;
+                    break;
+                case "finish":
+                    page_index = 2;
+                    break;
+            }
+            $('#index').sliderPro('gotoSlide', page_index);
         }
+        if ($(this).data("anchor-start") !== "undefined") {
+            goto("['data-anchor-end=" + $(this).data("anchor-start") + "']");
+        }
+    });
+    $(".palette-link").on("click", function () {
+        $("body").css("background-color", $(this).data("color"));
     });
 });

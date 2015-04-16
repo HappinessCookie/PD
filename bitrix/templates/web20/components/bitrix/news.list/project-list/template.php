0ceb5f4f006b2edec025fe7b977abf72d19f32e7
HappinessCookie <faartur@gmail.com>
2015-04-16 23:17:47 +0500

    fix resize image

--- bitrix/templates/web20/components/bitrix/news.list/project-list/template.php
+++ bitrix/templates/web20/components/bitrix/news.list/project-list/template.php
@@ -1,5 +1,5 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
-<? CModule::IncludeModule('iblock'); ?>
+<? //CModule::IncludeModule('iblock'); ?>
 
 <? foreach ($arResult["ITEMS"] as $arItem) { ?>
     <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6 project-preview inline-block" data-id="<?= $arItem["ID"]; ?>">
@@ -7,11 +7,11 @@
         $img = CFile::ResizeImageGet(
             $arItem["PREVIEW_PICTURE"],
             array(
-                "WIDTH" => 250,
-                "HEIGHT" => 190
+                "width" => 350,
+                "height" => 350
             ),
-            BX_RESIZE_IMAGE_EXACT,
-            false
+            BX_RESIZE_IMAGE_PROPORTIONAL_ALT,
+            true
         );
         ?>
         <img src="<?= $img["src"]; ?>" alt="" class="img-responsive table-bordered col-centered">

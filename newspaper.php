3b89b49dfa5005c6f1555ebd9b1bbb40d31398db
Денис <bikrevdeniis@mail.ru>
2015-04-16 20:26:43 +0300

    add page newspaper

---
+++ newspaper.php
@@ -0,0 +1,9 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+?>
+    <div class="container page">
+
+    </div>
+<?
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
+?>

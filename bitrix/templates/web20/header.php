4145bf7619f2ee508c76049142db9926bcf130b3
HappinessCookie <faartur@gmail.com>
2015-04-16 23:19:31 +0500

    Grid changed. Add item in to dropdown

--- bitrix/templates/web20/header.php
+++ bitrix/templates/web20/header.php
@@ -1,5 +1,4 @@
 <? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die(); ?>
-<? header("Content-Type: text/html; charset=utf-8"); //Нужно будет удалить ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -33,14 +32,14 @@
         <? /*$APPLICATION->ShowPanel();*/ ?>
     </div>
 <? endif ?>
-<div class="header navbar navbar-default navbar-fixed-top">
-    <div class="container side-pad">
-        <div class="col15-lg-6 col15-md-6 hidden-sm hidden-xs menu-contact">
+<div class="header navbar navbar-inverse navbar-fixed-top container">
+    <div class="side-pad">
+        <div class="col-lg-5 col-md-5 hidden-sm hidden-xs menu-contact">
             <div class="first-phone"><b>8 (917) 430-47-35</b></div>
             <div class="second-phone">8 (917) 430-35-49</div>
             <div class="address">г.Уфа ул.Сагита Агиша 16/1 оф.7</div>
         </div>
-        <div class="col15-lg-3 col15-md-3 col15-sm-7 col15-xs-7">
+        <div class="col-lg-2 col-md-2 col-sm-4 col-xs-10">
             <img class="logo img-responsive" src="/bitrix/templates/web20/images/logo.png">
         </div>
         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#responsive-menu">
@@ -49,22 +48,69 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
         </button>
-        <div class="col15-lg-6 col15-md-6 col15-sm-8 col15-xs-15 navbar-collapse text-center pull-right collapse"
+        <div class="col-lg-5 col-md-5 col-sm-12 col-xs-12 navbar-collapse text-center pull-right collapse"
              id="responsive-menu"
              aria-expanded="false">
             <ul class="nav navbar-nav navbar-right">
-                <li class="anchor" data-page="main"><a>ГЛАВНАЯ</a></li>
-                <li class="anchor" data-page="about"><a>О НАС</a></li>
-                <li class="btn-group menu-group-item">
-                    <a data-page="finish" type="button" class="btn btn-link anchor" style="padding-right: 0; float: left;">ГОТОВОЕ РЕШЕНИЕ</a>
-                    <a type="button"
-                       data-toggle="dropdown"
-                       class="btn btn-link dropdown-toggle pull-left"
-                       aria-expanded="false"><span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                        <li><a href="partners.php" target="_blank">ПРОЕКТЫ ПАРТНЕРОВ</a></li>
-                    </ul>
-                </li>
+                <? if (($_SERVER["REQUEST_URI"] == "/partners.php") || ($_SERVER["REQUEST_URI"] == "/newspaper.php")) { ?>
+                    <li class="anchor" data-page="main"><a href="/#index/main">ГЛАВНАЯ</a></li>
+                    <li class="btn-group menu-group-item">
+                        <a data-page="about"
+                           type="button"
+                           href="/#index/about"
+                           class="btn btn-link anchor"
+                           style="padding-right: 0; float: left;">О НАС</a>
+                        <a type="button"
+                           data-toggle="dropdown"
+                           class="btn btn-link dropdown-toggle pull-left"
+                           aria-expanded="false"><span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="partners.php">АРХГАЗЕТА</a></li>
+                        </ul>
+                    </li>
+                    <li class=" btn-group menu-group-item">
+                        <a data-page="finish"
+                           type="button"
+                           class="btn btn-link anchor"
+                           href="/#index/finish"
+                           style="padding-right: 0; float: left;">ГОТОВОЕ РЕШЕНИЕ</a>
+                        <a type="button"
+                           data-toggle="dropdown"
+                           class="btn btn-link dropdown-toggle pull-left"
+                           aria-expanded="false"><span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="partners.php" target="_blank">ПРОЕКТЫ ПАРТНЕРОВ</a></li>
+                        </ul>
+                    </li>
+                <? } else { ?>
+                    <li class="anchor" data-page="main"><a>ГЛАВНАЯ</a></li>
+                    <li class="btn-group menu-group-item">
+                        <a data-page="about"
+                           type="button"
+                           class="btn btn-link anchor"
+                           style="padding-right: 0; float: left;">О НАС</a>
+                        <a type="button"
+                           data-toggle="dropdown"
+                           class="btn btn-link dropdown-toggle pull-left"
+                           aria-expanded="false"><span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="partners.php">АРХГАЗЕТА</a></li>
+                        </ul>
+                    </li>
+                    <li class="btn-group menu-group-item">
+                        <a data-page="finish"
+                           type="button"
+                           class="btn btn-link anchor"
+                           style="padding-right: 0; float: left;">ГОТОВОЕ РЕШЕНИЕ</a>
+                        <a type="button"
+                           data-toggle="dropdown"
+                           class="btn btn-link dropdown-toggle pull-left"
+                           aria-expanded="false"><span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="partners.php">ПРОЕКТЫ ПАРТНЕРОВ</a></li>
+                        </ul>
+                    </li>
+                <? } ?>
             </ul>
         </div>
     </div>

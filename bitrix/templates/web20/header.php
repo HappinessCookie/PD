4989699a7bba5169fa5b6f50b272e354905b7ba4
HappinessCookie <faartur@gmail.com>
2015-04-29 13:02:39 +0500

    title changed

--- bitrix/templates/web20/header.php
+++ bitrix/templates/web20/header.php
@@ -4,13 +4,12 @@
 <head>
     <?
     $APPLICATION->ShowHead();
-    $APPLICATION->ShowTitle();
     $APPLICATION->ShowPanel();
     ?>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Projektstudio.ru</title>
+    <title><? $APPLICATION->ShowTitle("ProjektStudio") ?></title>
     <link rel="icon" href="bitrix/templates/web20/images/favicon.ico" type="image/x-icon">
     <link rel="shortcut icon" href="bitrix/templates/web20/images/favicon.ico" type="image/x-icon">
     <?

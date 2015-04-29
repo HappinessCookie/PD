415e92c3a578a594518348e599f70e5d865e6b00
HappinessCookie <faartur@gmail.com>
2015-04-29 11:17:54 +0500

    Pager disabled, count list unlimited

--- newspaper.php
+++ newspaper.php
@@ -1,6 +1,4 @@
-<?
-require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
-?>
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php"); ?>
     <div class="container page side-pad">
         <?$APPLICATION->IncludeComponent(
             "bitrix:news.list",
@@ -13,7 +11,7 @@ require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
                 "AJAX_MODE" => "Y",
                 "IBLOCK_TYPE" => "-",
                 "IBLOCK_ID" => "14",
-                "NEWS_COUNT" => "12",
+                "NEWS_COUNT" => "",
                 "SORT_BY1" => "ID",
                 "SORT_ORDER1" => "desc",
                 "SORT_BY2" => "",
@@ -42,9 +40,9 @@ require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
                 "CACHE_GROUPS" => "Y",
                 "PAGER_TEMPLATE" => "project-list-pager",
                 "DISPLAY_TOP_PAGER" => "N",
-                "DISPLAY_BOTTOM_PAGER" => "Y",
+                "DISPLAY_BOTTOM_PAGER" => "N",
                 "PAGER_TITLE" => "",
-                "PAGER_SHOW_ALWAYS" => "Y",
+                "PAGER_SHOW_ALWAYS" => "N",
                 "PAGER_DESC_NUMBERING" => "N",
                 "PAGER_DESC_NUMBERING_CACHE_TIME" => "36000",
                 "PAGER_SHOW_ALL" => "N",
@@ -55,6 +53,4 @@ require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
             false
         );?>
     </div>
-<?
-require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php");
-?>
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>

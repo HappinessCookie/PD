ca93d6ca82293900804930ec516743d1197ff333
Денис <bikrevdeniis@mail.ru>
2015-04-29 09:49:35 +0300

    change title, yandex metrika

--- bitrix/templates/web20/footer.php
+++ bitrix/templates/web20/footer.php
@@ -87,5 +87,37 @@ if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 <script src="<?= SITE_TEMPLATE_PATH ?>/js/lightbox.js"></script>
 <script src="<?= SITE_TEMPLATE_PATH ?>/js/ekko-lightbox.min.js"></script>
 <script src="<?= SITE_TEMPLATE_PATH ?>/js/function.js"></script>
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">(function (d, w, c) {
+        (w[c] = w[c] || []).push(function () {
+            try {
+                w.yaCounter29989784 = new Ya.Metrika({
+                    id: 29989784,
+                    webvisor: true,
+                    clickmap: true,
+                    accurateTrackBounce: true,
+                    trackHash: true
+                });
+            } catch (e) {
+            }
+        });
+        var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () {
+            n.parentNode.insertBefore(s, n);
+        };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else {
+            f();
+        }
+    })(document, window, "yandex_metrika_callbacks");</script>
+<noscript>
+    <div>
+        <img src="//mc.yandex.ru/watch/29989784" style="position:absolute; left:-9999px;" alt=""/>
+    </div>
+</noscript>
+<!-- /Yandex.Metrika counter -->
 </body>
 </html>
--- bitrix/templates/web20/header.php
+++ bitrix/templates/web20/header.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>xD</title>
+    <title>Projektstudio.ru</title>
     <!-- Bootstrap -->
     <link href="<?= SITE_TEMPLATE_PATH ?>/css/bootstrap.css" rel="stylesheet">
     <link href="<?= SITE_TEMPLATE_PATH ?>/css/font-awesome.css" rel="stylesheet">

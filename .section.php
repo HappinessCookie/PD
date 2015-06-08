2eb55247b383c412943cb40900ae69000b0b7013
HappinessCookie <faartur@gmail.com>
2015-06-08 12:24:37 +0500

    keywords and description change

---
+++ .section.php
@@ -0,0 +1,8 @@
+<?
+$sSectionName = "Главная";
+$arDirProperties = array(
+   "description" => "ProjektStudio",
+   "keywords" => "Проектирование, малоэтажное проектирование, продажа проектов, жилые дома, архитектура, проект дома",
+   "robots" => "index, follow"
+);
+?>
